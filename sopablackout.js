b2f2b583347c356b9984cf120535919379efbbd1
shu zOMG chen <shu.chen@freelancedreams.com>
2012-01-10 20:52:11 -1000

    mvp done

--- sopablackout.js
+++ sopablackout.js
@@ -14,7 +14,7 @@
 		}
 	};
 	var onDomReady = function(fn){
-		if (document.addEventListener) {
+		if (document.addEventListener){
 			document.addEventListener('DOMContentLoaded', fn, false);
 		}else{
 			if (!document.uniqueID && document.expando){return;};
@@ -44,32 +44,73 @@
 				curleft += obj.offsetLeft;
 				curtop += obj.offsetTop;
 			}while(obj = obj.offsetParent);
-			return [curleft, curtop];
 		}
-	}
+		return [curleft, curtop];
+	};
+	var txt = function(s){
+		return document.createTextNode(s);
+	};
+	var create = function(e, props){
+		var elem = document.createElement(e);
+		var props = props !== null ? props : {};
+		for (var key in props){
+			if (key == 'href'){
+				elem.href = props[key];
+			}else{
+				elem.style[key] = props[key];
+			}
+		}
+		l = arguments.length;
+		for (var i=2; i<l; i++){
+			elem.appendChild(arguments[i]);
+		}
+		return elem;
+	};
 
 	SopaBlackout.VERSION = '0.1.0';
+	SopaBlackout.MIN_HEIGHT = 100;
+	SopaBlackout.HEADER_TEXT = "This is what the web could look like under the Stop Online Piracy Act.";
 	SopaBlackout.blackout = function(obj_id){
 		var obj;
 		var body = document.body;
-		if (typeof obj_id === 'undefined'){
+		if (obj_id === false){
 			obj = body;
+			height = "100%";
 		}else{
 			obj = document.getElementById(obj_id);
+			var height = parseInt(getStyle(obj, 'height'), 10);
+			height = height > SopaBlackout.MIN_HEIGHT ? height : SopaBlackout.MIN_HEIGHT;
 		}
 		var offsets = findPos(obj);
-		var height = getStyle(obj, 'height');
-		var blackout = document.createElement('div');
-		blackout.appendChild(document.createTextNode("Hi there"));
-		blackout.style.position = "absolute";
-		blackout.style.top = offsets[1];
-		blackout.style.width = "100%";
-		blackout.style.height = height;
-		blackout.style.backgroundColor = "black";
+
+		var blackout = create('div', {
+				position: 'absolute',
+				top: offsets[1],
+				width: '100%',
+				backgroundColor: 'black',
+				textAlign: 'center',
+				paddingTop: '10px',
+				height: height,
+				color: '#999'},
+			create('h1', {color: '#999'}, txt(SopaBlackout.HEADER_TEXT)),
+			txt("Keep the web open. "),
+			create('a', {href: "http://google.com/"}, txt("Contact your representatives")),
+			txt(" or "),
+			create('a', {href: "http://yahoo.com/"}, txt("find out more"))
+		)
+
+		addEvent(blackout, 'click', function(e){
+			body.removeChild(blackout);
+		});
+
 		body.appendChild(blackout);
 	};
 	SopaBlackout.go = function(){
-		SopaBlackout.blackout(sopablackout_id);
+		if (typeof spoablackout_id === 'undefined'){
+			SopaBlackout.blackout(false);
+		}else{
+			SopaBlackout.blackout(sopablackout_id);
+		}
 	};
 
 	onDomReady(SopaBlackout.go);

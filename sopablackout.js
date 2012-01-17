e0243add64233983f190b093a879006899b56b9b
shu zOMG chen <shu.chen@freelancedreams.com>
2012-01-16 21:30:12 -1000

    sopablackout now supports firing on date

--- sopablackout.js
+++ sopablackout.js
@@ -85,20 +85,43 @@
 		}
 		return elem;
 	};
+	var getOpts = function(){
+		var ret = {};
+		for (var key in SopaBlackout.DEFAULTS){
+			var k = 'sopablackout_' + key;
+			ret[key] = (typeof window[k] === 'undefined') ? SopaBlackout.DEFAULTS[key] : window[k];
+		}
+		return ret;
+	};
+	var dateMatches = function(spec){
+		spec.push(false); spec.push(false); spec.push(false);
+		var today = new Date();
+		if ((spec[0] !== false && today.getFullYear() !== spec[0]) || 
+				(spec[1] !== false && today.getMonth() + 1 !== spec[1]) ||
+				(spec[2] !== false && today.getDate() !== spec[2])){
+			return false;
+		}
+		return true;
+	};
 
 	SopaBlackout.VERSION = '0.1.0';
 	SopaBlackout.MIN_HEIGHT = 100;
 	SopaBlackout.HEADER_TEXT = "This is what the web could look like under the Stop Online Piracy Act.";
 	SopaBlackout.CONTINUE_TEXT = "(click anywhere to continue)";
 	SopaBlackout.ZINDEX = Math.pow(2, 31) - 2;
-	SopaBlackout.blackout = function(obj_id, srsbzns){
+	SopaBlackout.DEFAULTS = {
+		'id': false,
+		'srsbzns': false,
+		'on': false
+	};
+	SopaBlackout.blackout = function(opts){
 		var obj;
 		var body = document.body;
-		if (obj_id === false){
+		if (opts['id'] === false){
 			obj = body;
 			height = "100%";
 		}else{
-			obj = document.getElementById(obj_id);
+			obj = document.getElementById(opts['id']);
 			var height = parseInt(getStyle(obj, 'height'), 10);
 			height = height > SopaBlackout.MIN_HEIGHT ? height : SopaBlackout.MIN_HEIGHT;
 		}
@@ -121,7 +144,7 @@
 				txt(" or "),
 				create('a', {href: "http://sopablackout.org/learnmore"}, txt("find out more")))
 		);
-		if (srsbzns !== true){
+		if (opts['srsbzns'] !== true){
 			blackout.appendChild(create('p', {paddingTop: '250px', color: '#333'}, txt(SopaBlackout.CONTINUE_TEXT)));
 			addEvent(blackout, 'click', function(e){
 				body.removeChild(blackout);
@@ -130,9 +153,11 @@
 		body.appendChild(blackout);
 	};
 	SopaBlackout.go = function(){
-		var obj_id = (typeof sopablackout_id === 'undefined') ? false : sopablackout_id;
-		var srsbzns = (typeof sopablackout_srsbzns === 'undefined') ? false : sopablackout_srsbzns;
-		SopaBlackout.blackout(obj_id, srsbzns);
+		var opts = getOpts();
+		if (opts['on'] !== false && !dateMatches(opts['on'])){
+			return;
+		}
+		SopaBlackout.blackout(opts);
 	};
 
 	onDomReady(SopaBlackout.go);
